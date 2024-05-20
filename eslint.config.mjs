587434f294863892196c518f479eb1cb82cc8fa5
rohanmonnat <135211340+rohanmonnat@users.noreply.github.com>
2024-05-21 00:18:50 +0530

    Add: jest.config.js to eslint ignore patterns

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,6 @@
 import eslint from '@eslint/js';
 import teslint from 'typescript-eslint';
 
-export default teslint.config(
-  eslint.configs.recommended,
-  ...teslint.configs.strict,
-  ...teslint.configs.stylistic,
-  {
-    ignores: ['dist', 'tests', 'node_modules'],
-  }
-);
+export default teslint.config(eslint.configs.recommended, {
+  ignores: ['dist', 'tests', 'node_modules', 'jest.config.js'],
+});
